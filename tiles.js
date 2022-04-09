7e98bb1c78bbff93be26f46580f924c32f3582da
nullparrot <github@weist.dev>
2022-04-09 11:25:52 -0700

    Load tiles from json, hard loas chapter 1 lesson 2

--- tiles.js
+++ tiles.js
@@ -1,20 +1,53 @@
 /*Create tiles based on lesson*/
-function makeTiles(tilesJSON,chapter,lesson){
-  tilesArray = tilesJSON.chapter_1.lesson_1
-  console.log("Chapter 1, Lesson 1",tilesArray)
+function makeTiles(tilesJSON, chapter, lesson) {
+  console.log("The JSON", tilesJSON);
+  tiles = tilesJSON.chapter_2.lesson_1;
+  console.log("Chapter 1, Lesson 1", tiles);
+  tiles.forEach((tile) => {
+    tileCount = tile.quantity;
+    tempcount = 0;
+    while (tempcount < tileCount) {
+      whiteboard = document.getElementById("tiles");
+      newtile = document.createElement("p");
+      newtile.setAttribute("class", "dragMe");
+      newtile.setAttribute(
+        "id",
+        tile.value + "-" + tile.color + "-" + tempcount
+      );
+      newtile.setAttribute(
+        "style",
+        "background-color:" +
+          tile.color +
+          ";" +
+          "border: 10px solid " +
+          tile.color +
+          ";"
+      );
+      newtile.innerHTML = tile.value;
+      whiteboard.appendChild(newtile);
+      tempcount = tempcount + 1;
+    }
+  });
+  /*make iems with class "dragMe" moveable*/
+  findMoveables("dragMe", "tiles");
 }
 
-fetch("https://raw.githubusercontent.com/nullparrot/tiles_content/main/tiles_content.json")
-  .then((tilesTMP) =>{
-    return tilesTMP.json()
-  })
-  .then((tiles) =>{
-    makeTiles(tiles,1,1)
-  })
+function reset(){
+  document.getElementById('tiles').innerHTML = ''
+}
 
+fetch(
+  "https://raw.githubusercontent.com/nullparrot/tiles_content/main/tiles_content.json"
+)
+  .then((tilesTMP) => {
+    return tilesTMP.json();
+  })
+  .then((tiles) => {
+    makeTiles(tiles, 1, 1);
+  });
 
 /* makes element given as argument moveable*/
-function makeMoveables(moveID,divID) {
+function makeMoveables(moveID, divID) {
   var dragItem = document.getElementById(moveID);
   var container = document.getElementById(divID);
 
@@ -80,15 +113,12 @@ function makeMoveables(moveID,divID) {
 }
 
 /* makes all elements with given class name movable if they have unique ids*/
-function findMoveables(className,divID) {
+function findMoveables(className, divID) {
   let moveIDcollection = document.getElementsByClassName(className);
   let moveIDarray = Array.from(moveIDcollection);
   console.log("List of ID's", moveIDarray);
   moveIDarray.forEach((element) => {
     console.log("Here's an ID", element.id);
-    makeMoveables(element.id,divID);
+    makeMoveables(element.id, divID);
   });
 }
-
-/*make iems with class "dragMe" moveable*/
-findMoveables("dragMe","tiles");
